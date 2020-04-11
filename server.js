7b222ac01c0d09db5497b336fbbd8ef8c1bbdb72
Ben Tait <ben.tait@uapcompany.com>
2020-04-11 21:26:04 +1000

    added jimp

--- server.js
+++ server.js
@@ -331,10 +331,11 @@ app.post('/process_memory_images',(req,res) =>{
     db.select('memfiles.fileurl','memfiles.ishero')
     .from('memfiles')
     .where({memid:memoryid})
-    .then(record =>  {
+    .then(record =>  record.json()
+    .then(record => {
         console.log('memory : ' + memoryid);
         console.log('highres image : ' , + record.fileurl + ' hero = ' + record.ishero);
-    })
+    }))
     
 })
 
